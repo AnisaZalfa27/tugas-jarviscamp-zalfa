c91fb4394c20c212f6eab4f45d8ba3209580e6f2
ZalfaZulfa27 <zkhn223@gmail.com>
2024-06-08 09:39:34 +0700

    tugas pekan 8 template admin

---
+++ mantu/app/Http/Controllers/DetailController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DetailController extends Controller
+{
+    //mendapatkan dan menampilkan detail/deskripsi tugas
+
+    public function index(){
+        
+    }
+}
--- mantu/routes/web.php
+++ mantu/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template');
 
 });
 
 route::get('/task', [TaskController::class,'index']);
+Route::get('/tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
